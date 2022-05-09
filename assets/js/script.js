d1b35e4cafbfbf7bd9ba1fb59c7bd576c1493b4b
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2022-05-09 13:14:59 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-
 const scrollElement = document.getElementById("landing__counter");
 const heroShow = document.querySelector(".hero__article");
 const counterContainer = document.getElementById("counter");
-let timeOut = f => f;
 let countFlag = false;
 let heroFlag = true;
 
@@ -20,7 +18,7 @@ const handleScrollAnimationCounter = () => {
     counterContainer.classList.add("showcounter")
     countFlag = true;
 
-    animateValue(scrollElement, 300000, 400000, 1200);
+    animateValue(scrollElement, 250000, 400000, 1800);
   }
 };
 
@@ -41,16 +39,11 @@ function animateValue(obj, start, end, duration) {
 
 window.addEventListener("scroll", () => {
   if(heroFlag && window.screen.width > 700){
-    clearTimeout(timeOut)
       heroFlag = false
       heroShow.classList.add("shownow")
   }else if(document.body.getBoundingClientRect().top === 0 && heroFlag === false){
-
-    timeOut = setTimeout(()=> {
-      heroShow.classList.remove("shownow")
-    }, 7000)
-  }else if(heroShow.getBoundingClientRect().bottom < 100){
     heroFlag = true
+    heroShow.classList.remove("shownow")
   }
   handleScrollAnimationCounter();
 });
